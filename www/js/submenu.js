d2a8d4c5d1b4ad123aedbef959a6caec698c6de0
Emily Clark <eclark8395@gmail.com>
2020-11-21 14:38:35 -0600

    working on submenu for mobile

--- www/js/submenu.js
+++ www/js/submenu.js
@@ -1,17 +1,17 @@
-function toggleItem() {
-    if (this.classList.contains("proj-dropdown-content")) {
-      this.classList.remove("proj-dropdown-content");
-    } else if (menu.querySelector(".proj-dropdown-content")) {
-      menu.querySelector(".proj-dropdown-content").classList.remove("proj-dropdown-content");
-      this.classList.add("proj-dropdown-content");
-    } else {
-      this.classList.add("proj-dropdown-content");
-    }
-  }
+// function toggleItem() {
+//     if (this.classList.contains("proj-dropdown-content")) {
+//       this.classList.remove("proj-dropdown-content");
+//     } else if (menu.querySelector(".proj-dropdown-content")) {
+//       menu.querySelector(".proj-dropdown-content").classList.remove("proj-dropdown-content");
+//       this.classList.add("proj-dropdown-content");
+//     } else {
+//       this.classList.add("proj-dropdown-content");
+//     }
+//   }
    
-  /* Event listeners */
-  for (let item of items) {
-      if (item.querySelector(".proj-dropdown")) {
-        item.addEventListener("click", toggleItem, false);
-      }   
-  }
+//   /* Event listeners */
+//   for (let item of items) {
+//       if (item.querySelector(".proj-dropdown")) {
+//         item.addEventListener("click", toggleItem, false);
+//       }   
+//   }
